1696e5a0f431396fc1940f836bf78da92e511611
GaneshRoyal07 <kondetiganesh43@gmail.com>
2026-02-09 13:01:36 +0530

    functions

---
+++ functions.py
@@ -0,0 +1,10 @@
+#calling a function
+def my_function():
+    print("Hello from a function")
+my_function()
+
+#function with return value
+def add(a,b):
+    return a+b
+result=add(3,4)
+print(result)
